23069350e9da562ca32b71444594d5f6b38cbd9a
Jaskaran Singh <s.jaskaran4@gmail.com>
2018-01-22 23:39:00 +0000

    finished step 4

---
+++ getHTML.js
@@ -0,0 +1,40 @@
+var https = require('https'); 
+
+function getHTML (options, callback) {
+
+  var stream = "";
+
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length);
+    stream += data;
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.');
+    callback(stream);
+  });
+
+}); 
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);

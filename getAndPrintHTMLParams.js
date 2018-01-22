36c8dc354f6ecc5beff05e82bb491158b41f3d99
Jaskaran Singh <s.jaskaran4@gmail.com>
2018-01-22 23:32:26 +0000

    third step finished

---
+++ getAndPrintHTMLParams.js
@@ -0,0 +1,36 @@
+var https = require('https'); 
+
+function getAndPrintHTML (options) {
+
+  var stream = "";
+
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length);
+    stream += data;
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.');
+    console.log('Output stream:\n', stream);
+  });
+
+}); 
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
